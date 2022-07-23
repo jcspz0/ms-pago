304ae9a47ba76c531ca0a3de3317c94e6f598136
Juan Carlos <jcspz0stardan@gmail.com>
2022-07-23 08:31:43 -0400

    Comprobando

--- ms-pago-webapi/src/main/java/com/diplo/webapi/mspago/MsPagoWebapiApplication.java
+++ ms-pago-webapi/src/main/java/com/diplo/webapi/mspago/MsPagoWebapiApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 )
 public class MsPagoWebapiApplication extends SpringBootServletInitializer {
 
+	//add
 	public static void main(String[] args) {
 		SpringApplication.run(MsPagoWebapiApplication.class, args);
 	}
